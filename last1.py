53de9f8d7858410d70f4cd23f5fecf3997699bde
Maruthi20012001 <165935925+Maruthi20012001@users.noreply.github.com>
2024-05-09 23:33:58 +0530

    Update last1.py

--- last1.py
+++ last1.py
@@ -2,7 +2,7 @@ import streamlit as st
 import time
 import io
 
-st.title("SUDOKU SOLVER BY MARUTHI D.M UNDER THE SUPERVISION OF Dr K.N DAS")
+st.title("SUDOKU SOLVER BY MARUTHI D.M UNDER SUPERVISION OF Dr K.N DAS")
 
 # Function to read Sudoku from an uploaded file
 def read_sudoku_from_uploaded_file(uploaded_file):
